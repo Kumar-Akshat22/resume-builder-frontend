c0e112cdf3b561259a1ef3d8b41684ca21ae2530
Kumar Akshat <kakshat247@gmail.com>
2024-04-11 00:33:09 +0530

    towards implementing the work experience form

--- src/components/ResumeDetails.js/Experience.js
+++ src/components/ResumeDetails.js/Experience.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Fab from "@mui/material/Fab"
+import AddIcon from "@mui/icons-material/Add"
+
 
 function Experience() {
-  return (
-    <div>Experience</div>
-  )
+
+    const [description , setDescription] = useState([])
+    return (
+        <div className='w-full p-5 mt-6'>
+
+            <div className='max-w-[1140px] mx-auto'>
+
+                <div className='mb-4'>
+                    <p className='uppercase text-xl'>Experience</p>
+                    <span className='text-sm'>List your work experience, most recent first</span>
+                </div>
+
+                <div className='flex flex-col gap-5'>
+
+                    {/* Company Name and Job Title */}
+                    <div className='flex gap-5 align-items-center justify-center'>
+                        <label className='w-[50%]'>
+                            <span className=''>Employer</span>
+                            <input type='text' placeholder='e.g. John' className='w-full border p-[8px] rounded-[0.2rem] text-slate-400 mt-1'></input>
+                        </label>
+
+                        <label className='w-[50%]'>
+                            <span>Job Title</span>
+                            <input type='text' placeholder='e.g. Williams' className='w-full border p-[8px] rounded-[0.2rem] mt-1'></input>
+                        </label>
+                    </div>
+
+                    {/* Duration */}
+                    <label>
+
+                        <span>Duration</span>
+                        <input type='text' placeholder='e.g. Williams' className='w-full border p-[8px] rounded-[0.2rem] mt-1'></input>
+                    </label>
+
+                    {/* Bullet Points */}
+
+                    <label>
+
+                        <span>Bullet Points <span className='text-sm'>(Add max 4 points)</span></span>
+
+                        <div className='flex gap-6'>
+                            <input type='text' placeholder='e.g. Led a team of 10 members' className='w-full border p-[8px] rounded-[0.2rem] mt-1'></input>
+                            <Fab color='primary' aria-label="add">
+                                <AddIcon />
+                            </Fab>
+                        </div>
+                    
+                    </label>
+                    
+
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Experience
--- src/pages/ResumeDetails.js
+++ src/pages/ResumeDetails.js
@@ -18,28 +18,28 @@ function ResumeDetails() {
             <NavLink to={'/resume-details/personal-details'}>
                 <span className={
                 `${detailType === 'personalDetails' 
-                ? 'bg-[#25A05D] text-white font-bold border'
+                ? 'bg-[#3983fa] text-white font-semibold border'
                 : 'bg-transparent'} text-xl rounded-md px-4 py-1 transition duration-200 ease-in-out`} onClick={()=>{setDetailType('personalDetails')}}>Personal Details</span>
             </NavLink>
 
             <NavLink to={'/resume-details/education'} className=''>
                 <span className={
                 `${detailType === 'education' 
-                ? 'bg-[#25A05D] text-white font-bold border'
+                ? 'bg-[#3983fa] text-white font-semibold border'
                 : 'bg-transparent'} text-xl rounded-md px-4 py-1 transition duration-200 ease-in-out`} onClick={()=>{setDetailType('education')}}>Education</span>
             </NavLink>
 
             <NavLink to={'/resume-details/experience'}>
                 <span className={
                 `${detailType === 'experience' 
-                ? 'bg-[#25A05D] text-white font-bold border'
+                ? 'bg-[#3983fa] text-white font-semibold border'
                 : 'bg-transparent'} text-xl rounded-md px-4 py-1 transition duration-200 ease-in-out`} onClick={()=>{setDetailType('experience')}}>Work Experience</span>
             </NavLink>
             
             <NavLink to={'/resume-details/projects'}>
                 <span className={
                 `${detailType === 'projects' 
-                ? 'bg-[#25A05D] text-white font-bold border'
+                ? 'bg-[#3983fa] text-white font-semibold border'
                 : 'bg-transparent'} text-xl rounded-md px-4 py-1 transition duration-200 ease-in-out`} onClick={()=>{setDetailType('projects')}}>Projects</span>
             </NavLink>
 
@@ -48,7 +48,7 @@ function ResumeDetails() {
             <NavLink to={'/resume-details/extra'}>
                 <span className={
                 `${detailType === 'extras' 
-                ? 'bg-[#25A05D] text-white font-bold border'
+                ? 'bg-[#3983fa] text-white font-semibold border'
                 : 'bg-transparent'} text-xl rounded-md px-4 py-1 transition duration-200 ease-in-out`} onClick={()=>{setDetailType('extras')}}>Extras</span>
             </NavLink>
 
